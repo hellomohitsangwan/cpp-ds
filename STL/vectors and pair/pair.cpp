18048a0a220183f6f6b8e741ddaea4afb5371256
hellomohitsangwan <hellomohitsangwan@gmail.com>
2021-06-16 18:32:13 +0530

    pairs new type added

--- STL/vectors and pair/pair.cpp
+++ STL/vectors and pair/pair.cpp
@@ -1,4 +1,3 @@
-#include<bits/stdc++.h>
 #include<iostream>
 using namespace std;
 
@@ -13,6 +12,9 @@ int main() {
     pair<int , char>p2;
     p2 = make_pair(8 , 'h');
     cout<<p2.first<<" "<<p2.second;
-    
+
+    //or we can create in this way also
+    pair<int , int> p3 = {4 , 8};
+        cout<<p3.first<<" "<<p3.second;
     return 0;
 }
