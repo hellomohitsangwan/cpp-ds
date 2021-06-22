d82bdecf4fa1be0abc0aedcadd4e682d9f15e38d
hellomohitsangwan <hellomohitsangwan@gmail.com>
2021-06-22 12:12:45 +0530

    cat-2 girish question added

---
+++ vit exam questions/cat-2_computeDistance.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<iomanip>
+#include <math.h>
+using namespace std;
+
+double coumputeDistance(int x1 , int y1 , int x2 , int y2) {
+    // double d = //dou yout calc. here
+    return d;
+}
+double coumputeDistance(int x1 , int y1 , int z1 , int x2 , int y2 , int z2) {
+    // double d = //dou yout calc. here
+    return d;
+}
+int main() {
+    int x1 ,y1 , z1 , x2 , y2 , z2;
+    cin>>x1>>y1>>z1>>x2>>y2>>z2;
+    cout<<fixed<<setprecision(2)<<coumputeDistance(x1 , y1 , x2 , y2)<<endl;
+    cout<<fixed<<setprecision(2)<<coumputeDistance(x1 , y1 , z1 , x2 , y2 , z2)<<endl;
+}
