27cf0920d2262c3710efe87cbf3f848261ef130a
hellomohitsangwan <hellomohitsangwan@gmail.com>
2021-08-06 21:48:47 +0530

    optimized

--- algorithms/hashing/count_distinct_elements.cpp
+++ algorithms/hashing/count_distinct_elements.cpp
@@ -19,7 +19,17 @@ int distinctElements_BruteForce(int a[] , int s) {
     return count;
 }
 
+//optimized
+int distinctElements_Optimized(int a[] , int s) {
+    set<int> se;
+    for (int i = 0; i < s; i++) {
+        se.insert(a[i]); 
+    }
+    return se.size();
+}
+
 int main() {
     int a[] = { 0, 1, 2, 1 , 3 , 2 , 4 , 1 , 4};
-    cout<<distinctElements_BruteForce(a , 9);
+    cout<<distinctElements_BruteForce(a , 9)<<endl;
+    cout<<distinctElements_Optimized(a , 9);
 }
