9fd7bd54500d6427f891dfc426af7ec4a5bec9e3
hellomohitsangwan <hellomohitsangwan@gmail.com>
2021-09-09 18:44:04 +0530

    explanation comments added

--- stack/Prefix postfix infix evaluation/Prefix_expression_evaluation.cpp
+++ stack/Prefix postfix infix evaluation/Prefix_expression_evaluation.cpp
@@ -1,13 +1,21 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+/*
+1. We start from the last character of the string and traverse the string from right to left.
+2. If the current character is a digit, we push it into the stack.
+3. If the current character is an operator, we pop two elements from the stack. The first pop is the second operand and the next pop is the first operand.
+ We perform the operation on the two operands and push the result back to the stack.
+4. When we reach the start of the string, the result will be inside the stack. We return the top of the stack as the result of the expression.
+*/
+// cpp
+
 int convert(string s) {
     stack<int> st;
     for (int i = s.length() - 1; i >= 0 ; i--) {
         if(s[i] <= '9' && s[i] >= '0') {
             st.push(s[i] - '0');
-        }
-        else {
+        } else {
             int o1 = st.top();
             st.pop();
             int o2 = st.top();
@@ -33,6 +41,7 @@ int convert(string s) {
     cout<<"result : ";
     return st.top();
 }
+
 
 int main() {
     string s ; cin >>s;
