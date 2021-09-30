ce4f75d87aa829ca1951783464f90d9ba35b0991
hellomohitsangwan <hellomohitsangwan@gmail.com>
2021-09-30 12:06:06 +0530

    order agnostic BS added

--- algorithms/binary search/code_implementation.cpp
+++ algorithms/binary search/code_implementation.cpp
@@ -18,7 +18,36 @@ int binarySearch(int a[] , int find , int len) {
     } return -1;  //element is not present in the array
 }
 
+int orderArgonsticBS(int a[] , int find , int l) {
+
+    bool isAsec = a[0] < a[l-1];
+
+    int start  = 0;
+    int end = l;
+    while(start <= end) {
+        int midIndex = start + (end - start)/2;
+        if(a[midIndex] == find) {
+            return midIndex;
+        }
+        if(isAsec) {
+        if(a[midIndex] > find) {  //element is present in the left side
+            end = midIndex - 1;
+        } else if(a[midIndex] < find) {  //element is present in the right side
+            start = midIndex + 1;
+        }
+        }
+         else {   //descnding order
+        if(a[midIndex] < find) {  //element is present in the right side
+            end = midIndex - 1;
+        } else if(a[midIndex] > find) {  //element is present in the left side
+            start = midIndex + 1;
+        } 
+        }
+    } return -1;  //element is not present in the array
+}
+
 int main() {
    int a[] = {1 , 3, 5, 7,99};
-   cout<<"the element is currently present int the arrya at index : "<<binarySearch(a , 99 , 4);
+   int b[] = {43 , 21 , 10 , 1 , 0};
+   cout<<"the element is currently present int the arrya at index : "<<binarySearch(a , 99 , 4)<<endl<<orderArgonsticBS(b , 21 , 5);
 }
