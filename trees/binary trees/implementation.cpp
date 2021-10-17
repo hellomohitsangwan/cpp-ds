212004243a2209ef3ec54d16ee0379c1defb26a2
mohit kumar <hellomohitsangwan@gmail.com>
2021-10-17 16:07:41 +0530

    trees start

--- trees/binary trees/implementation.cpp
+++ trees/binary trees/implementation.cpp
@@ -19,5 +19,14 @@ int main() {
   root->left = new Node(2);
   root->right = new Node(3);
   root->left->left = new Node(4);
-  root->left->left = new Node(5);
+  root->left->right = new Node(5);
+  root->right->left = new Node(6);
+  root->right->right = new Node(7);
 }
+/*
+    Tree structure:
+
+    1
+  2   3
+4  5 6  7
+*/
