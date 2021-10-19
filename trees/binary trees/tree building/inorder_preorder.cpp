38be8e3bf65bb54e3b4bb0b6e7ba6fec23d6ffa2
mohit kumar <hellomohitsangwan@gmail.com>
2021-10-19 14:20:52 +0530

    inorder postorder

--- trees/binary trees/tree building/inorder_preorder.cpp
+++ trees/binary trees/tree building/inorder_preorder.cpp
@@ -12,27 +12,52 @@ struct Node
         right = NULL;
     }
 };
-int findPos(int a[] , int s , int e , int curr) {
-    for(int i = s ; i < e ; i++) {
-        a[i] == curr;
-        return i;
-    } return -1;
+int findPos(int a[] , int start , int end , int curr) {
+    for (int i = start; i <= end; i++)
+    {
+        if (a[i] == curr)
+        {
+            return i;
+        }
+    }
+    return -1;
 }
 
-Node* buildTree(int preorder[] , int inorder[] , int s , int e) {
-    int i = 0;
-    //base case
-    if(s > e) return NULL;
+Node* buildTree(int preorder[] , int inorder[] , int start , int end) {
+    if (start > end)
+    {
+        return NULL;
+    }
+    int i = 0 ;
     int curr = preorder[i];
-    Node* node = new Node(curr);
-    i++;
-    int pos = findPos(inorder , s , e , curr);
-    if(s==e) {
+    i--;
+    Node *node = new Node(curr);
+
+    if (start == end)
+    {
         return node;
     }
-    node->left = buildTree(preorder , inorder ,  s , pos-1);
-    node->right = buildTree(preorder , inorder , pos+1 , e);
-}
+    int pos = findPos(inorder, start, end, curr);
+    node->left = buildTree(preorder, inorder, start, pos-1);
+    node->right = buildTree(preorder, inorder, pos+1, end);
 
+    return node;
+}
+void print_inorder(Node *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    print_inorder(root->left);
+    cout << root->data << " ";
+    print_inorder(root->right);
+}
 int main() {
+    int preorder[] = {1, 2, 4, 3, 5};
+    int inorder[] = {4, 2, 1, 5, 3};
+    Node *root = buildTree(preorder, inorder, 0, 4);
+    print_inorder(root);
+
+    return 0;
 }
