8a0ce2673ebd63f8c129eb8ff5b17ff397f9bd7b
mohit kumar <hellomohitsangwan@gmail.com>
2021-10-27 04:39:34 +0530

    shortest

---
+++ graphs/shortest distance questions/undirected_graphs_with_unit_weights.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<vector>
+#include<queue>
+using namespace std;
+
+
+void BFS(vector<int> adj[] , int src , int n) {
+    queue<int> q;
+    vector<int> dist(n , INT_MAX);
+    q.push(src);
+    while(!q.empty()) {
+        int node = q.front();
+        q.pop();
+        for(auto e : adj[node]) {
+            if(dist[e] > dist[node] + 1) {  //lesser possibe dist. found
+                dist[e] = dist[node] + 1;
+                q.push(e);
+            }
+        }
+    }
+    for(auto e : dist) {
+        cout<<e<< " ";
+    }
+}
+
+
+int main() {
+  
+}
