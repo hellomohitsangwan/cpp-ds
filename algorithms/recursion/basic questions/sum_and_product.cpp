40fcc25112c0cf4652c37189faba67b2e410e431
mohit kumar <hellomohitsangwan@gmail.com>
2021-11-10 16:40:07 +0530

    product with comments

--- algorithms/recursion/basic questions/sum_and_product.cpp
+++ algorithms/recursion/basic questions/sum_and_product.cpp
@@ -6,7 +6,11 @@ int sumOfDigits(int n) {
     if(n == 0) return n;
     return sumOfDigits(n/10) + n%10;
 }
-
+int productOfDigits(int n) {
+    if(n%10 == n) return n;  //if only 1 digit left then return that digit
+    return productOfDigits(n/10) * (n%10);
+}
 int main() {
-  cout<<sumOfDigits(124);
+  cout<<sumOfDigits(124)<<endl;
+  cout<<productOfDigits(1234);
 }
