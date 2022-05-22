35102c576b33815574366a24d83955c933e5866b
mohit kumar <hellomohitsangwan@gmail.com>
2022-05-22 22:40:52 +0530

    selection sort recursion

--- algorithms/recursion/pattern questions/reverse_triangle.cpp
+++ algorithms/recursion/pattern questions/reverse_triangle.cpp
@@ -2,6 +2,15 @@
 #include<vector>
 using namespace std;
 
+//iterative
+void print_Rev_tri_Ite(int r , int c) {
+    for(int i  = r ; i > 0 ; i--) {
+        for(int j = 0 ; j < i ; j++) {
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+}
 //Recursive
 //  r = 4, c = 0;
 // ****
@@ -39,19 +48,28 @@ void bubbleSortRecur(vector<int> &a , int r , int c) {
     }
 }
 
-//iterative
-void print_Rev_tri_Ite(int r , int c) {
-    for(int i  = r ; i > 0 ; i--) {
-        for(int j = 0 ; j < i ; j++) {
-            cout<<"*";
+void selecctionSortRecur(vector<int> &a , int r , int c , int maxI) {
+    if(r == 0) {
+        return;
+    }
+    if(r > c) {
+        if(a[c] > a[maxI]) {
+            selecctionSortRecur(a , r , ++c , c);
+        } else {
+            selecctionSortRecur(a , r , ++c , maxI);
         }
-        cout<<endl;
+    } else {
+        int temp = a[maxI];
+        a[maxI] = a[r-1];
+        a[r-1] = temp;
+        selecctionSortRecur(a , --r , 0 , 0);
     }
 }
+
 int main() {
     // print_Rev_tri_Ite(4 , 0);
     vector<int> a = {5 , 3 , 9 , 7, 2};
-    bubbleSortRecur(a  , 5, 0);
+    bubbleSortRecur(a  , 5, 0 );
     for (int i = 0; i < 5; i++)
     {
         cout<<a[i]<<" ";
