4e3631c55bcef9bc493229e9bd14cd3621a94a8f
mohit kumar <hellomohitsangwan@gmail.com>
2022-05-23 04:14:27 +0530

    new good ques

--- algorithms/recursion/string subsets questions/printAllSubsets.cpp
+++ algorithms/recursion/string subsets questions/printAllSubsets.cpp
@@ -24,3 +24,28 @@ void printAllSubsetsAsciiAlso(string p , string up) {
 int main() {
   printAllSubsetsAsciiAlso("" , "abc");
 }
+
+
+
+
+/*
+Count the number of perfect squares upto N
+Time Complexity: O(Sqrt(N))
+
+#include<iostream>
+using namespace std;
+
+int countSquares(int n) {
+    int j=1;
+    for(int i=1; i<n;  j++){
+        i=(j+1)*(j+1);
+    }
+    return j-1;
+}
+
+int main() {
+    int n = 12;
+    cout<<"Squares till "<<n<<": "<<countSquares(n);
+    return 0;
+}
+*/
