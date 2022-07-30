77995676aee5549ced8b289dc10e028a04fd4435
mohit kumar <hellomohitsangwan@gmail.com>
2022-07-30 23:56:49 +0530

    print only one answer trick

--- algorithms/recursion/imp question/subsequence_sum_eaquals_to_k.cpp
+++ algorithms/recursion/imp question/subsequence_sum_eaquals_to_k.cpp
@@ -23,10 +23,33 @@ void printSubsequecne(int i , vector<int> ds , int cs , int ts , int a[] , int l
     printSubsequecne(i+1 , ds , cs , ts , a , l);
 } 
 
+//Trick to return only one subsequence or one answer nd avoid future recursion calls
+bool printOneSubsequecne(int i , vector<int> ds , int cs , int ts , int a[] , int l) {
+    if(i == l) {
+        if(cs == ts) {
+            for(auto e:ds) {
+                cout<<e<< " ";
+            }
+            return true;
+        } else {
+            return false;
+        }
+    }
+    // C-1 : taking the element
+    ds.push_back(a[i]);
+    cs += a[i];
+    if(printOneSubsequecne(i+1 , ds , cs , ts , a , l) == true) return true;
+    cs -= a[i];
+    ds.pop_back();
+    // C-2 : not taking the element
+    if(printOneSubsequecne(i+1 , ds , cs , ts , a , l) == true) return true;
+    return false;
+} 
+
 int main() {
   int a[] = {1 , 2 , 1};
   vector<int> ds;
   int ts = 2;
   int l = 3;
-  printSubsequecne(0 , ds , 0 , ts , a , l);
+  printOneSubsequecne(0 , ds , 0 , ts , a , l);
 }
