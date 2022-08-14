bd2fa30543e2682cc3523f79f83da91f37ba2dad
mohit kumar <hellomohitsangwan@gmail.com>
2022-08-15 03:15:01 +0530

    memoization  sol.

--- algorithms/Dynamic programming/max_sum_of_nonAdjacent_elements.cpp
+++ algorithms/Dynamic programming/max_sum_of_nonAdjacent_elements.cpp
@@ -12,7 +12,22 @@ int maxrec(int a[] , int n , int ind) {
     return max(pick , notPick);
 }
 
+int maxDp(int a[] , int n , int ind , vector<int> dp) {
+    if(ind == 0) return a[0];
+    if(ind < 0) return 0;
+    if(dp[ind] != -1) return dp[ind];
+
+    int pick = a[ind] + maxDp(a , n , ind - 2 , dp);
+    int notPick = 0 + maxDp(a , n , ind - 1 , dp);
+
+    return dp[ind] = max(pick , notPick);
+}
+
 int main() {
   int a[4] = {2 , 1 , 4 , 9};
   cout<<maxrec(a , 4 , 3);
+
+  vector<int> dp(4, -1);
+  cout<<maxDp(a , 4 , 3 , dp);
+
 }
